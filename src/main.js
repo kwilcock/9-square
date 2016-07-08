00a5123b98e70bf60f1e5db9c9d693136f8f1620
Tourtle21 <kardlargin@hotmail.com>
2016-07-07 23:50:24 -0600

    Adds configuration files
    https://github.com/kwilcock/9-square/issues/1

---
+++ src/main.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var ReactDOM = require("react-dom");
+var React = require("react");
+var App = require("./components/App")
+
+ReactDOM.render(<App />, document.getElementById("app"))
