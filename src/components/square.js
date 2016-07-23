2045a33e16847a57ab0c5aca95d8c6646f577e24
Tourtle21 <kardlargin@hotmail.com>
2016-07-23 11:12:40 -0600

    created ssquare components for game component

---
+++ src/components/square.js
@@ -0,0 +1,13 @@
+var React = require("react");
+
+
+var Square = React.createClass({
+  render: function () {
+    return (
+      <div>
+        Square
+      </div>
+    )
+  }
+})
+module.exports = Square;
