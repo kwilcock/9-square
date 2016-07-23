f44eeed31f010c6241d4ce0900cba0b2415807e5
Tourtle21 <kardlargin@hotmail.com>
2016-07-23 10:37:48 -0600

    added two child components game and pattern to app

---
+++ src/components/game.js
@@ -0,0 +1,12 @@
+var React = require("react");
+
+var Game = React.createClass({
+  render: function () {
+    return (
+      <div>
+        Game
+      </div>
+    )
+  }
+})
+module.exports = Game;
